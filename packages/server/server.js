501a98710ad9e8f7cde5c25994e253ad3f1f2d55
Rémy Sabliet <sabliet@gmail.com>
2024-03-02 19:20:50 +0900

    initialization mono repo pnpm + ui + server packages

--- packages/server/server.js
+++ packages/server/server.js
@@ -2,10 +2,11 @@
 // where your node app starts
 
 // init project
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const path = require("path");
 
 app.use(bodyParser());
 app.use(morgan());
@@ -14,14 +15,13 @@ app.use(morgan());
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/views/index.html');
-});
+
+app.use(express.static(path.resolve(__dirname, '../../dist')));
+
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
---
+++ packages/ui/vite.config.js
@@ -0,0 +1,10 @@
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+// vite.config.js
+export default {
+  plugins: [reactRefresh()],
+  esbuild: {},
+  build: {
+    outDir: "../../dist",
+  },
+};
